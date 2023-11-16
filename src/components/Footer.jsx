61248fb02f655404c10ee68f37f74a9f0047ef4d
unknown <dev.edu@hotmail.com>
2023-11-15 22:24:31 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <p>&copy; 2023 Your Eduardo Correia Developer. All rights reserved. | 
+            <a href="https://github.com/Eduardo1985S">Terms of Service</a> | 
+            <a href="https://github.com/Edutechtreinamentos">Privacy Policy</a>
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div><h2>Calculator 2.0</h2></div>
+  )
+}
+
+export default Header
